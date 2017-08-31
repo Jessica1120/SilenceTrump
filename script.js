5f9f289ce3a3f208f51ee4221c1494d6aaf948b3
Jessica Brown <jessicakbrown1120@gmail.com>
2017-08-31 17:25:14 -0500

    For loop

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ console.log('Am I here?')
 function silenceTrump(tweet) {
     var convert = tweet.split("");
     console.log(convert);
-   
+    for(var i = 0; i <= convert.length-1; i++) {
+        console.log(convert[i]);
+    }
 }
 silenceTrump("test")
